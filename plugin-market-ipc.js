1c6f16ea41429b827b15530a83d3812d037af438
wuqian <71761124+guanyibei1314@users.noreply.github.com>
2026-08-17 16:29:54 +0800

    feat: expose installed plugin state to market

--- plugin-market-ipc.js
+++ plugin-market-ipc.js
@@ -1,14 +1,36 @@
 'use strict'
 
+const fs = require('fs')
 const path = require('path')
 const { pathToFileURL } = require('url')
 const { app, ipcMain } = require('electron')
-const { loadPluginCatalog } = require('./plugin-market')
+const { isPackageName, loadPluginCatalog } = require('./plugin-market')
 
 let registered = false
 
 function pluginMarketCachePath() {
   return path.join(app.getPath('userData'), 'plugin-market-cache.json')
+}
+
+function profilePackagePath() {
+  return path.join(app.getPath('userData'), 'dsh-home', 'profiles', 'web', 'package.json')
+}
+
+function installedPackages() {
+  try {
+    const pkg = JSON.parse(fs.readFileSync(profilePackagePath(), 'utf8'))
+    const names = new Set()
+    for (const key of ['dependencies', 'optionalDependencies', 'devDependencies']) {
+      const deps = pkg && pkg[key]
+      if (!deps || typeof deps !== 'object' || Array.isArray(deps)) continue
+      for (const name of Object.keys(deps)) {
+        if (isPackageName(name)) names.add(name)
+      }
+    }
+    return Array.from(names).sort((a, b) => a.localeCompare(b))
+  } catch (_) {
+    return []
+  }
 }
 
 function isAuthorizedSender(event) {
@@ -27,8 +49,13 @@ function registerPluginMarketIpc() {
     if (!isAuthorizedSender(event)) throw new Error('unauthorized sender')
     return loadPluginCatalog(pluginMarketCachePath())
   })
+  ipcMain.handle('plugin:installed', (event) => {
+    if (!isAuthorizedSender(event)) throw new Error('unauthorized sender')
+    return installedPackages()
+  })
 }
 
 module.exports = {
+  installedPackages,
   registerPluginMarketIpc,
 }
